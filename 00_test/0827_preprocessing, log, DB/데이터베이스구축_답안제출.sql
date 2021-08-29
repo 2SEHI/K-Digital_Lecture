2739bc3dbfaf27fcbd9e5d8fb0ccce2864edba28
2SEHI <krsehi2@gmail.com>
2021-08-29 20:48:39 +0900

    과제 등록
    폴더 정리

---
+++ 00_test/0827_preprocessing, log, DB/데이터베이스구축_답안제출.sql
@@ -0,0 +1,136 @@
+-- 1 데이터베이스 구축
+CREATE TABLE USER_INFO( 
+	ID CHAR(18) PRIMARY KEY,
+	NAME CHAR(18), 
+	PASSWORD CHAR(18), 
+	EMAIL CHAR(30),
+	
+);
+
+CREATE TABLE BOARD( 
+	ARTICLE_ID NUMBER PRIMARY KEY,
+	USER_ID CHAR(18), 
+	ARTICLE_TITLE CHAR(50), 
+	ARTICLE_DETAIL VARCHAR2(1000), 
+	ARTICLE_HITS NUMBER, 
+	ARTICLE_RECOMMEND NUMBER, 
+	FOREIGN KEY(USER_ID) REFERENCES USER_INFO(ID)
+);
+
+CREATE TABLE FILES(
+    FILE_ID CHAR(18) PRIMARY KEY,
+    FILE_NAMe CHAR(18),
+    FILE_SIZE CHAR(18),
+    ARTICLE_ID NUMBER,
+    FOREIGN KEY(ARTICLE_ID) REFERENCES BOARD(ARTICLE_ID)
+);
+
+
+
+-- 2.데이터베이스 활용
+
+--DEPT 부서 테이블, DEPTNO가 부서 번호, DNAME이 부서명, LOC가 지역
+CREATE TABLE DEPT
+       (DEPTNO NUMBER(2) CONSTRAINT PK_DEPT PRIMARY KEY,
+	DNAME VARCHAR2(14) ,
+	LOC VARCHAR2(13) ) ;
+
+
+CREATE TABLE EMP
+   (EMPNO NUMBER(4) CONSTRAINT PK_EMP PRIMARY KEY,
+	ENAME VARCHAR2(10),
+	JOB VARCHAR2(9),
+	MGR NUMBER(4),
+	HIREDATE DATE,
+	SAL NUMBER(7,2),
+	COMM NUMBER(7,2),
+	DEPTNO NUMBER(2) CONSTRAINT FK_DEPTNO REFERENCES DEPT);
+);
+
+INSERT INTO DEPT VALUES(10,'ACCOUNTING','NEW YORK');
+INSERT INTO DEPT VALUES (20,'RESEARCH','DALLAS');
+INSERT INTO DEPT VALUES(30,'SALES','CHICAGO');
+INSERT INTO DEPT VALUES(40,'OPERATIONS','BOSTON');
+
+INSERT INTO EMP VALUES
+(7369,'SMITH','CLERK',7902,to_date('17-12-1980','dd-mm-yyyy'),800,NULL,20);
+INSERT INTO EMP VALUES
+(7499,'ALLEN','SALESMAN',7698,to_date('20-2-1981','dd-mm-yyyy'),1600,300,30);
+INSERT INTO EMP VALUES
+(7521,'WARD','SALESMAN',7698,to_date('22-2-1981','dd-mm-yyyy'),1250,500,30);
+INSERT INTO EMP VALUES
+(7566,'JONES','MANAGER',7839,to_date('2-4-1981','dd-mm-yyyy'),2975,NULL,20);
+INSERT INTO EMP VALUES
+(7654,'MARTIN','SALESMAN',7698,to_date('28-9-1981','dd-mm-yyyy'),1250,1400,30);
+INSERT INTO EMP VALUES
+(7698,'BLAKE','MANAGER',7839,to_date('1-5-1981','dd-mm-yyyy'),2850,NULL,30);
+INSERT INTO EMP VALUES
+(7782,'CLARK','MANAGER',7839,to_date('9-6-1981','dd-mm-yyyy'),2450,NULL,10);
+INSERT INTO EMP VALUES
+(7788,'SCOTT','ANALYST',7566,to_date('13-7-1987','dd-mm-yyyy')-85,3000,NULL,20);
+INSERT INTO EMP VALUES
+(7839,'KING','PRESIDENT',NULL,to_date('17-11-1981','dd-mm-yyyy'),5000,NULL,10);
+INSERT INTO EMP VALUES
+(7844,'TURNER','SALESMAN',7698,to_date('8-9-1981','dd-mm-yyyy'),1500,0,30);
+INSERT INTO EMP VALUES
+(7876,'ADAMS','CLERK',7788,to_date('13-7-1987','dd-mm-yyyy'),1100,NULL,20);
+INSERT INTO EMP VALUES
+(7900,'JAMES','CLERK',7698,to_date('3-12-1981','dd-mm-yyyy'),950,NULL,30);
+INSERT INTO EMP VALUES
+(7902,'FORD','ANALYST',7566,to_date('3-12-1981','dd-mm-yyyy'),3000,NULL,20);
+INSERT INTO EMP VALUES
+(7934,'MILLER','CLERK',7782,to_date('23-1-1982','dd-mm-yyyy'),1300,NULL,10);
+
+-- 2-1
+SELECT EMPNO, ENAME, JOB, SAL
+FROM EMP
+WHERE SAL >= 3000;
+
+-- 2-2
+SELECT ENAME, DEPTNO
+FROM EMP
+WHERE EMPNO = 7788;
+
+-- 2-3
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '%L%L%' 
+AND (DEPTNO = 30 OR MGR = 7782)
+
+-- 2-4
+select job as "업무", max(sal) as "최대 급여", min(sal) as "최소 급여", sum(sal)  as "급여의 합"
+from emp 
+group by job
+
+-- 2-5
+select emp.ename, emp.deptno, dept.dname
+from emp, dept
+
+--2-6
+select emp.empno as "사원번호", emp.hiredate as "관리자 입사일", mng.empno as "관리자 사원번호", mng.hiredate as "관리자 입사일"
+from emp, 
+-- 관리자의 사원번호와 입사일
+(select empno, hiredate
+from emp
+where JOB='MANAGER')  mng
+-- 사원의 관리자 번호와 관리자 사원번호가 같으면서
+-- 사원의 입사일이 관리자의 입사일보다 빠른 경우
+where emp.mgr = mng.empno and 
+emp.hiredate < mng.hiredate
+
+--2-7
+select ename, job, sal
+from emp
+where DEPTNO = 
+(select DEPTNO from dept where loc = 'DALLAS')
+
+--2-8
+insert into dept (DEPTNO, DNAME, LOC) values (50, 'SALES', 'SEOUL')
+
+--2-9
+drop table DEPT
+
+--2-10
+
+--1) view
+--2) index
